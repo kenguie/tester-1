600dddf4ca6bb52fdafc715a94b432fd843f3969
Kenrick Guie <nyhunter77@yahoo.com>
2017-10-14 15:10:22 -0400

    Initial Commit

---
+++ tester-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tester-1
+//
+//  Created by Kenrick Guie on 10/14/17.
+//  Copyright © 2017 Kenrick Guie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
